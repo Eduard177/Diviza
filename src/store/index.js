d50617a884c29e6de799e4672b3d3f80d329375f
Eduard <eduarro2001@gmail.com>
2020-06-30 14:37:41 -0400

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,26 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        loading: {
+            titulo: '',
+            estado: false,
+            color: 'primary'
+        }
+    },
+    mutations: {
+
+        mostrarLoading(state, payload) {
+            state.loading.titulo = payload.titulo
+            state.loading.estado = true
+            state.loading.color = payload.color
+        },
+
+        ocultarLoading(state) {
+            state.loading.estado = false
+        }
+
+    },
+    actions: {},
+    modules: {}
 })
